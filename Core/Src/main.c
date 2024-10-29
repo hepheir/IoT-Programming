1a9ae72f4e6158d1afd2838dd111cb9868bbf763
Hepheir <hepheir@gmail.com>
2024-10-30 04:34:05 +0900

    chore: P14를 pull-down으로 변경

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -233,7 +233,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : vibrtion_transducer_Pin */
   GPIO_InitStruct.Pin = vibrtion_transducer_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(vibrtion_transducer_GPIO_Port, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
